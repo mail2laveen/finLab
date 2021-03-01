d0585fe3e96fb162760f9b54e27343f25352c744
labin <mail2laveen@gmail.com>
2021-03-01 16:06:54 +0530

    modified

--- src/three.js/index.js
+++ src/three.js/index.js
@@ -18,7 +18,7 @@ import HostObject from './HostObject';
 import aws from './awspack';
 import anim from './animpack';
 import Amplify, { Auth } from 'aws-amplify';
-import awsconfig from './aws-exports';
+import awsconfig from '../aws-exports';
 Amplify.configure(awsconfig);
 
 export default {
